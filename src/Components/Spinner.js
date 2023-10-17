2d8483605745a0b15ca547f37321e092a83a537d
Harikrishnan14 <harikrishnankhd09@gmail.com>
2023-10-17 21:31:23 +0530

    Changed Class based App to Functional based App

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 import loading from './loading.gif'
 
-export class Spinner extends Component {
-  render() {
+const Spinner = () =>{
+
     return (
       <div className="text-center">
         <img className="my-3" style={{height: "30px"}} src={loading} alt="Loading"></img>
       </div>
     )
-  }
 }
 
 export default Spinner
