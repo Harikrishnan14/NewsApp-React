a935da0a518a6b6d710dd753bc7acd016857e167
Harikrishnan14 <harikrishnankhd09@gmail.com>
2023-10-17 20:27:15 +0530

    NewsApp - React

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+  render() {
+
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+
+    return (
+      <div className="my-3">
+        <div className="card">
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ zIndex: "1", left: "50%" }}>{source}</span>
+          <img src={!imageUrl ? "https://i.ytimg.com/vi/x1yntjc3f6M/hqdefault.jpg" : imageUrl} className="card-img-top" alt="..." style={{ height: "12rem" }} />
+          <div className="card-body">
+            <h5 className="card-title" style={{ height: "70px" }}>{title}...</h5>
+            <p className="card-text" style={{height: "75px"}}>{description}...</p>
+            <p className="card-text" style={{height: "50px"}}><small className="text-body-secondary">By {author ? author : "Unknown"} <br></br>On {new Date(date).toGMTString()}</small></p>
+            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
